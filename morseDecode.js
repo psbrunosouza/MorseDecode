6aca082f2f2e1b16efbbd13fe23c61f0b13299e4
psbrunosouza <45085894+psbrunosouza@users.noreply.github.com>
2019-03-15 14:15:08 -0300

    Create morseDecode.js

---
+++ morseDecode.js
@@ -0,0 +1,22 @@
+var str = '-... .-. ..- -. ---   -.. ---   .--. ';
+function morseDecode(sstr){
+  var word = '';
+  var aux = '';
+  for(var i in sstr){
+    
+      if((sstr[i] == ' ') && (sstr[i+1] == ' ') && (sstr[i+2] == ' ')){
+        aux += ' ';
+        continue;
+      }
+      
+      if((sstr[i] == ' ') && (sstr[i+1] != ' ')){
+        aux += morseCode[word];
+        word = '';
+        continue;
+      }
+      word += sstr[i];
+  }
+  return aux;
+}
+
+console.log(morseDecode(str));
